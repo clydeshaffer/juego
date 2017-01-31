796506b6c62c5b6e218c9c1ff49aa2d2db6ab241
Clyde Shaffer <clyde@ense.nyc>
2017-01-31 12:39:00 -0500

    tidy global scope a little

--- juego.js
+++ juego.js
@@ -34,208 +34,249 @@ Vector2.randomOnUnitCircle = function() {
 
 Vector2.zero = new Vector2(0, 0);
 
-function CirclePoints(n, r) {
-    var points = [];
-    var angleStep = 2 * Math.PI / n;
-    var i = 0;
-    for(i = 0; i < n; i++) {
-        points.push(new Vector2(Math.cos(i * angleStep) * r, Math.sin(i * angleStep) * r));
+var Juego = new (function () {
+
+
+    function CirclePoints(n, r) {
+        var points = [];
+        var angleStep = 2 * Math.PI / n;
+        var i = 0;
+        for(i = 0; i < n; i++) {
+            points.push(new Vector2(Math.cos(i * angleStep) * r, Math.sin(i * angleStep) * r));
+        }
+        return points;
     }
-    return points;
-}
 
-function randInt(x) {
-    return Math.floor(Math.random() * x);
-}
-
-var canvas = document.createElement("canvas");
-canvas.width = 800;
-canvas.height = 600;
-document.body.appendChild(canvas);
-var ctx = canvas.getContext("2d");
-
-function _x(v) { return v ? v.x : v};
-function _y(v) { return v ? v.y : v};
-
-function V_(someFunc) {
-    return function(v1, v2, v3, a, b, c, d, e) {
-        return someFunc(_x(v1), _y(v1),
-            _x(v2), _y(v2),
-            _x(v3), _y(v3),
-            a, b, c, d, e);
+    function randInt(x) {
+        return Math.floor(Math.random() * x);
     }
-}
 
-var vctx = {};
-var funcsToBind = [
-    "fillRect",
-    "strokeRect",
-    "lineTo",
-    "moveTo",
-    "translate",
-    "scale"
-];
+    var canvas = document.createElement("canvas");
+    canvas.width = 800;
+    canvas.height = 600;
+    document.body.appendChild(canvas);
+    var ctx = canvas.getContext("2d");
 
-function plot(v) {
-    vctx.fillRect(v, new Vector2(1, 1));
-}
+    function _x(v) { return v ? v.x : v};
+    function _y(v) { return v ? v.y : v};
 
-funcsToBind.forEach(function(funcName) {
-    vctx[funcName] = V_(ctx[funcName].bind(ctx));
-})
-
-function tracePoints(pointArray) {
-    ctx.beginPath();
-    vctx.moveTo(pointArray[0]);
-    pointArray.forEach(vctx.lineTo);
-    vctx.lineTo(pointArray[0]);
-    ctx.closePath();
-    ctx.stroke();       
-}
-
-
-function testPoints(a, b, c) {
-    if(Array.isArray(a)) return testPoints(a[0], a[1], a[2]);
-
-    var midAC = a.midpoint(c);
-    var bToMidAC = midAC.add(b.scale(-1));
-    return (bToMidAC.dot(midAC) < 0);
-}
-
-function bindArray(arr) {
-    return function(i) {
-        return arr[i];
+    function V_(someFunc) {
+        return function(v1, v2, v3, a, b, c, d, e) {
+            return someFunc(_x(v1), _y(v1),
+                _x(v2), _y(v2),
+                _x(v3), _y(v3),
+                a, b, c, d, e);
+        }
     }
-}
 
-function testConvex(pointArray) {
-    var arrfunc = bindArray(pointArray);
-    for(var i = pointArray.length; i < pointArray.length * 2; i ++) {
-        var iA = (i - 1) % pointArray.length;
-        var iB = i % pointArray.length;
-        var iC = (i + 1) % pointArray.length;
+    var vctx = {};
+    var funcsToBind = [
+        "fillRect",
+        "strokeRect",
+        "lineTo",
+        "moveTo",
+        "translate",
+        "scale"
+    ];
 
-        if(!testPoints([iA, iB, iC].map(arrfunc))) return false;
-
+    function plot(v) {
+        vctx.fillRect(v, new Vector2(1, 1));
     }
-    return true;
-}
 
-function mutate(pointArray, iterations, strength) {
-    var k = iterations * pointArray.length;
-    for(var i = 0; i < pointArray.length; i ++) {
-        var imod = i % pointArray.length;
+    funcsToBind.forEach(function(funcName) {
+        vctx[funcName] = V_(ctx[funcName].bind(ctx));
+    })
 
-        var oldPoint = pointArray[imod];
-        var offset = Vector2.randomOnUnitCircle().scale(strength);
-        pointArray[imod] = oldPoint.add(offset);
-        if(!testConvex(pointArray)) {
-            pointArray[imod] = oldPoint;
+    function tracePoints(pointArray) {
+        ctx.beginPath();
+        vctx.moveTo(pointArray[0]);
+        pointArray.forEach(vctx.lineTo);
+        vctx.lineTo(pointArray[0]);
+        ctx.closePath();
+        ctx.stroke();       
+    }
+
+
+    function testPoints(a, b, c) {
+        if(Array.isArray(a)) return testPoints(a[0], a[1], a[2]);
+
+        var midAC = a.midpoint(c);
+        var bToMidAC = midAC.add(b.scale(-1));
+        return (bToMidAC.dot(midAC) < 0);
+    }
+
+    function bindArray(arr) {
+        return function(i) {
+            return arr[i];
+        }
+    }
+
+    function testConvex(pointArray) {
+        var arrfunc = bindArray(pointArray);
+        for(var i = pointArray.length; i < pointArray.length * 2; i ++) {
+            var iA = (i - 1) % pointArray.length;
+            var iB = i % pointArray.length;
+            var iC = (i + 1) % pointArray.length;
+
+            if(!testPoints([iA, iB, iC].map(arrfunc))) return false;
+
+        }
+        return true;
+    }
+
+    function mutate(pointArray, iterations, strength) {
+        var k = iterations * pointArray.length;
+        for(var i = 0; i < pointArray.length; i ++) {
+            var imod = i % pointArray.length;
+
+            var oldPoint = pointArray[imod];
+            var offset = Vector2.randomOnUnitCircle().scale(strength);
+            pointArray[imod] = oldPoint.add(offset);
+            if(!testConvex(pointArray)) {
+                pointArray[imod] = oldPoint;
+            }
+
+        }
+    }
+
+    function SpriteSheet(url, columns, rows) {
+        this.rows = rows;
+        this.columns = columns;
+        this.frameCount = rows * columns;
+        this.ready = false;
+        this.imgElem = document.createElement("img");
+        this.imgElem.style = "display:none;";
+        var self = this;
+        this.imgElem.onload = function() {
+            self.ready = true;
+        }
+        document.body.appendChild(this.imgElem);
+        this.imgElem.src = url;
+    }
+
+    SpriteSheet.prototype.draw = function(frame) {
+        if(this.ready) {
+            var c = frame % this.columns;
+            var r = (frame - c) / this.columns;
+            var width = this.imgElem.width / this.columns;
+            var height = this.imgElem.height / this.rows;
+            ctx.drawImage(this.imgElem, c * this.imgElem.width / this.columns, r * this.imgElem.height / this.rows, width,  height, 0, 0, width, height);
+        }
+    }
+
+    function doNothing() {}
+
+    function RenderNode() {
+    this.position = new Vector2(0, 0);
+    this.rotation = 0;
+    this.scale = new Vector2(1,1);
+    this.children = [];
+    this.points = [];
+    this.sprite = null;
+    this.spriteFrame = 0;
+    this.strokeStyle = "black";
+    this.fillStyle = null;
+    this.update = doNothing;
+    this.traversalFlag = false; //security against cyclic RenderNode hierarchy
+    }
+
+    RenderNode.prototype.updateTree = function(traversal) {
+        if(this.traversalFlag == traversal) return;
+        this.traversalFlag = traversal;
+        this.update();
+        this.children.forEach(function(child) {
+            child.updateTree(traversal);
+        });
+    }
+
+    RenderNode.prototype.renderTree = function(traversal) {
+        if(this.traversalFlag == traversal) return;
+        this.traversalFlag = traversal;
+        ctx.save();    
+        vctx.translate(this.position);
+        ctx.rotate(this.rotation);
+        vctx.scale(this.scale); 
+        if(this.sprite) {
+            this.sprite.draw(this.spriteFrame);
+        }
+        if(this.strokeStyle) {
+            ctx.strokeStyle = this.strokeStyle;
+            tracePoints(this.points);
+        }
+        if(this.fillStyle) {
+            ctx.fillStyle = this.fillStyle;
+            ctx.fill();
+        }
+        this.children.forEach(function(child) {
+            child.renderTree();
+        });
+        ctx.restore();
+    }
+
+    var worldTree = new RenderNode();
+
+    var previousRenderTime = 0;
+    var traversalFlag = true;
+
+    var Time = {
+        deltaTime : 0,
+        totalTime : 0
+    }
+
+    function InputAxis(negativeKey, positiveKey) {
+        this.positiveKey = positiveKey;
+        this.negativeKey = negativeKey;
+
+        this.positiveKeyPressed = 0;
+        this.negativeKeyPressed = 0;
+
+        var self = this;
+        function makeKeyUpdate(keyStatus) {
+            return function(event) {
+                if(event.keyCode == positiveKey) {
+                    self.positiveKeyPressed = keyStatus;
+                } else if(event.keyCode == negativeKey) {
+                    self.negativeKeyPressed = keyStatus;
+                }
+            }
         }
 
+        window.addEventListener("keydown", makeKeyUpdate(1));
+        window.addEventListener("keyup", makeKeyUpdate(0));
     }
-}
 
-function SpriteSheet(url, columns, rows) {
-    this.rows = rows;
-    this.columns = columns;
-    this.frameCount = rows * columns;
-    this.ready = false;
-    this.imgElem = document.createElement("img");
-    this.imgElem.style = "display:none;";
-    var self = this;
-    this.imgElem.onload = function() {
-        self.ready = true;
+    InputAxis.prototype.sample = function() {
+        return this.positiveKeyPressed - this.negativeKeyPressed;
     }
-    document.body.appendChild(this.imgElem);
-    this.imgElem.src = url;
-}
 
-SpriteSheet.prototype.draw = function(frame) {
-    if(this.ready) {
-        var c = frame % this.columns;
-        var r = (frame - c) / this.columns;
-        var width = this.imgElem.width / this.columns;
-        var height = this.imgElem.height / this.rows;
-        ctx.drawImage(this.imgElem, c * this.imgElem.width / this.columns, r * this.imgElem.height / this.rows, width,  height, 0, 0, width, height);
+    function drawFrame(timestamp) {
+        var deltaT = timestamp - previousRenderTime;
+        ctx.clearRect(0, 0, 800, 600);
+        Time.totalTime = timestamp;
+        Time.deltaTime = deltaT;
+        worldTree.updateTree(traversalFlag);
+        traversalFlag = !traversalFlag;
+        worldTree.renderTree(traversalFlag);
+        traversalFlag = !traversalFlag;
+        previousRenderTime = timestamp;
+        window.requestAnimationFrame(drawFrame);
     }
-}
 
-function doNothing() {}
-
-function RenderNode() {
-this.position = new Vector2(0, 0);
-this.rotation = 0;
-this.scale = new Vector2(1,1);
-this.children = [];
-this.points = [];
-this.sprite = null;
-this.spriteFrame = 0;
-this.strokeStyle = "black";
-this.fillStyle = null;
-this.update = doNothing;
-this.traversalFlag = false; //security against cyclic RenderNode hierarchy
-}
-
-RenderNode.prototype.updateTree = function(traversal) {
-    if(this.traversalFlag == traversal) return;
-    this.traversalFlag = traversal;
-    this.update();
-    this.children.forEach(function(child) {
-        child.updateTree(traversal);
-    });
-}
-
-RenderNode.prototype.renderTree = function(traversal) {
-    if(this.traversalFlag == traversal) return;
-    this.traversalFlag = traversal;
-    ctx.save();    
-    vctx.translate(this.position);
-    ctx.rotate(this.rotation);
-    vctx.scale(this.scale); 
-    if(this.sprite) {
-        this.sprite.draw(this.spriteFrame);
+    function initRenderTimer(timestamp) {
+        previousRenderTime = timestamp;
+        window.requestAnimationFrame(drawFrame);
     }
-    if(this.strokeStyle) {
-        ctx.strokeStyle = this.strokeStyle;
-        tracePoints(this.points);
-    }
-    if(this.fillStyle) {
-        ctx.fillStyle = this.fillStyle;
-        ctx.fill();
-    }
-    this.children.forEach(function(child) {
-        child.renderTree();
-    });
-    ctx.restore();
-}
 
-var worldTree = new RenderNode();
+    window.requestAnimationFrame(initRenderTimer);
 
-var previousRenderTime = 0;
-var traversalFlag = true;
 
-var Time = {
-    deltaTime : 0,
-    totalTime : 0
-}
-
-function drawFrame(timestamp) {
-    var deltaT = timestamp - previousRenderTime;
-    ctx.clearRect(0, 0, 800, 600);
-    Time.totalTime = timestamp;
-    Time.deltaTime = deltaT;
-    worldTree.updateTree(traversalFlag);
-    traversalFlag = !traversalFlag;
-    worldTree.renderTree(traversalFlag);
-    traversalFlag = !traversalFlag;
-    previousRenderTime = timestamp;
-    window.requestAnimationFrame(drawFrame);
-}
-
-function initRenderTimer(timestamp) {
-    previousRenderTime = timestamp;
-    window.requestAnimationFrame(drawFrame);
-}
-
-window.requestAnimationFrame(initRenderTimer);
+    this.CirclePoints = CirclePoints;
+    this.randInt = randInt;
+    this.RenderNode = RenderNode;
+    this.InputAxis = InputAxis;
+    this.SpriteSheet = SpriteSheet;
+    this.Time = Time;
+    this.worldTree = worldTree;
+    this.ctx = ctx;
+    this.vctx = vctx;
+})();
--- juegoDemo.js
+++ juegoDemo.js
@@ -1,18 +1,18 @@
-var ball = new RenderNode();
+var ball = new Juego.RenderNode();
 
-ball.points = CirclePoints(16, 16);
+ball.points = Juego.CirclePoints(16, 16);
 
 ball.position = new Vector2(200, 200);
 
-worldTree.children.push(ball);
+Juego.worldTree.children.push(ball);
 
 ball.position.x = 100;
 
-var miniBall = new RenderNode();
+var miniBall = new Juego.RenderNode();
 
 miniBall.position = new Vector2(0, -24);
 
-miniBall.points = CirclePoints(16, 8);
+miniBall.points = Juego.CirclePoints(16, 8);
 
 ball.children.push(miniBall);
 
@@ -20,16 +20,16 @@ ball.children.push(miniBall);
 var prevBall = miniBall;
 
 var wiggle = function() {
-    this.rotation = Math.sin(Time.totalTime / 1000) * Math.PI / 8;
+    this.rotation = Math.sin(Juego.Time.totalTime / 1000) * Math.PI / 8;
 };
 
 ball.update = wiggle;
 miniBall.update = wiggle;
 
 function growTentacle() {
-    var newBall = new RenderNode();
+    var newBall = new Juego.RenderNode();
     newBall.position = new Vector2(0, -16);
-    newBall.points = CirclePoints(16, 8);
+    newBall.points = Juego.CirclePoints(16, 8);
     newBall.update = wiggle;
     prevBall.children.push(newBall);
     prevBall = newBall;
@@ -41,15 +41,15 @@ growTentacle();
 growTentacle();
 
 
-var rockmanSprite = new SpriteSheet('http://i.imgur.com/zyQUbWo.png', 10, 7);
+var rockmanSprite = new Juego.SpriteSheet('http://i.imgur.com/zyQUbWo.png', 10, 7);
 
-var rockman = new RenderNode();
+var rockman = new Juego.RenderNode();
 rockman.position = new Vector2(300,300);
 rockman.sprite = rockmanSprite;
 rockman.update = function() {
-    this.spriteFrame = Math.floor(Time.totalTime / 125) % 70;
+    this.spriteFrame = Math.floor(Juego.Time.totalTime / 125) % 70;
 }
 
-worldTree.children.push(rockman);
+Juego.worldTree.children.push(rockman);
 
-ctx.imageSmoothingEnabled = false;
+Juego.ctx.imageSmoothingEnabled = false;
